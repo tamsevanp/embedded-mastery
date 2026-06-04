4e2f81d18653e30ccf18083eaf56f9b2ace8506c
tamsevanp <tamsevanp@gmail.com>
2026-06-04 22:04:54 +0530

    bit ops

---
+++ C programming/03_bit_manipulation_and_register_access/01_bit_ops.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdint.h>
+
+#define SET_BIT(mask,pos)   ((mask) |= (1U << (pos)))
+#define CLEAR_BIT(mask,pos)  ((mask) &= (~(1U << (pos))))
+#define TOGGLE_BIT(mask,pos)  ((mask) ^= (1U << (pos)))
+#define CHECK_BIT(mask,pos)  (((mask) >> (pos)) & 1U) //((mask) & (1U << (pos)))
+
+int main()
+{
+    uint8_t num = 154; //10011010
+
+    uint8_t value = SET_BIT(num,2);
+    printf("Number given: %d \n",value);
+
+    return 0;
+}
